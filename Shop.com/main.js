54d5c6c6d47a5b8529e591cbcc25a4605109ace4
buikiet08 <89330660+buikiet08@users.noreply.github.com>
2021-12-07 14:07:41 +0700

    up

--- Shop.com/main.js
+++ Shop.com/main.js
@@ -1,32 +1,41 @@
-var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+// product
+var swiper = new Swiper(".product-container", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+});
+if(window.innerWidth <= 1023) {
   var swiper = new Swiper(".product-container", {
-    slidesPerView: 4,
+    slidesPerView: 2,
     spaceBetween: 30,
   });
-  if(window.innerWidth <= 490) {
-    var swiper = new Swiper(".product-container", {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    });
-  }
-
+}else if(window.innerWidth < 739 ) {
+  var swiper = new Swiper(".product-container", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+  });
+}
+// company
+var swiper = new Swiper(".company", {
+  slidesPerView: 5,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+if(window.innerWidth <= 1023) {
   var swiper = new Swiper(".company", {
-    slidesPerView: 5,
+    slidesPerView: 4,
     pagination: {
       el: ".swiper-pagination",
     },
   });
+}else if(window.innerWidth < 739 ) {
+  var swiper = new Swiper(".company", {
+    slidesPerView: 2,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+}
 
 const btnbar = document.querySelector('.bar-menu');
 const btnclose = document.querySelector('.btn-close-menu');
