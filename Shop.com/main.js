f8bfabc55072d9e9a27168ccb7399f04d38b86d6
buikiet08 <89330660+buikiet08@users.noreply.github.com>
2021-12-08 09:01:49 +0700

    up

--- Shop.com/main.js
+++ Shop.com/main.js
@@ -46,6 +46,39 @@ btnbar.addEventListener('click', (e) => {
     document.querySelector('.menu').classList.add('activemenu')
 })
 btnclose.addEventListener('click', () => {
-  console.log('jajjaa')
   document.querySelector('.menu').classList.remove('activemenu')
 })
+// scroll menu
+const menu = document.querySelector('.menu');
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY >= 300) {
+    menu.classList.add('active');
+  }else{
+    menu.classList.remove('active');
+  }
+  // màn hình tablet & mobile remove active
+  if(window.innerWidth <= 1023) {
+    menu.classList.remove('active');
+  }
+})
+// scroll menu tablet & mobile
+const menutablet = document.querySelector('.header-top');
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY >= 300) {
+    menutablet.classList.add('activemobile');
+  }else{
+    menutablet.classList.remove('activemobile');
+  }
+})
+// to top
+const btntop = document.querySelector('.toTop');
+window.addEventListener('scroll', () => {
+  if(window.scrollY >= 600) {
+    btntop.classList.add('activeTop');
+  }else{
+    btntop.classList.remove('activeTop');
+  }
+})
+  
